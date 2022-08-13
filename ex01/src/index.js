94b142c0bced6e34577c4848888ac9952ba779b5
Ajla Fijuljanin <ajlafi@maestralsolutions.com>
2022-08-13 11:29:15 +0200

    Repo08

---
+++ ex01/src/index.js
@@ -0,0 +1,6 @@
+// Write code below this line
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+// Write code above this line
+module.exports = reusableFunction;
---
+++ ex02/src/index.js
@@ -0,0 +1,7 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+    console.log (name, age); 
+    }
+    myFunction(“John”, “23”);
+// Write code above this line
+module.exports = functionPrintInfo;
